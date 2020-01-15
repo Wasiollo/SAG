61a154989f87e6b6bb9b99f3284b8b3fc84e0585
Aleksander Zamojski <zamojski96@gmail.com>
2020-01-15 17:26:37 +0100

    Add FindBrokerAgent behaviour

--- src/main/java/com/sag/pagent/agents/ShopAgent.java
+++ src/main/java/com/sag/pagent/agents/ShopAgent.java
@@ -1,9 +1,11 @@
 package com.sag.pagent.agents;
 
 import com.sag.pagent.behaviors.ReceiveMessagesBehaviour;
-import com.sag.pagent.messages.RegisterShopAgent;
 import com.sag.pagent.services.ServiceType;
+import com.sag.pagent.services.ServiceUtils;
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.TickerBehaviour;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
@@ -11,6 +13,7 @@ import jade.lang.acl.UnreadableException;
 
 public class ShopAgent extends BasicAgent {
     ReceiveMessages receiveMessages;
+    AID brokerAgent;
 
     @Override
     protected void addServices(DFAgentDescription dfd) {
@@ -30,6 +33,7 @@ public class ShopAgent extends BasicAgent {
         super.setup();
         receiveMessages = new ReceiveMessages(this);
         addBehaviour(receiveMessages);
+        addBehaviour(new FindBrokerAgent(this, 1000));
     }
 
     private class ReceiveMessages extends ReceiveMessagesBehaviour {
@@ -44,4 +48,23 @@ public class ShopAgent extends BasicAgent {
         }
 
     }
+
+    public class FindBrokerAgent extends TickerBehaviour {
+        public FindBrokerAgent(Agent a, long timeout) {
+            super(a, timeout);
+        }
+
+        @Override
+        protected void onTick() {
+            brokerAgent = ServiceUtils.findAgent(myAgent, ServiceType.BROKER);
+
+            if (brokerAgent == null) {
+                log.debug("{} agent not found", ServiceType.BROKER);
+                return;
+            }
+            log.debug("{} agent found", ServiceType.BROKER);
+            removeBehaviour(this);
+        }
+    }
+
 }
