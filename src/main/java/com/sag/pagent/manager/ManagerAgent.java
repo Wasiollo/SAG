f2744d5563ca8719338271cb6a258ce6cd50a8ef
Wasiollo <wasiak.mk@gmail.com>
2020-01-27 20:11:49 +0100

    Adding purchaseOrders to list in ManagerAgent

--- src/main/java/com/sag/pagent/manager/ManagerAgent.java
+++ src/main/java/com/sag/pagent/manager/ManagerAgent.java
@@ -6,9 +6,17 @@ import com.sag.pagent.manager.messages.PurchaseOrder;
 import com.sag.pagent.services.ServiceType;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.UnreadableException;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 public class ManagerAgent extends BasicAgent {
     private ReceiveMessagesBehaviour receiveMessages;
+    private List<PurchaseOrder> purchaseOrders = new ArrayList<>();
 
     @Override
     protected void addServices(DFAgentDescription dfd) {
@@ -36,9 +44,19 @@ public class ManagerAgent extends BasicAgent {
 
         if (content instanceof PurchaseOrder) {
             logReceivedMessage(msg, PurchaseOrder.class);
-//            handlePurchaseOrder(msg);
+            handlePurchaseOrder(msg);
         } else {
             receiveMessages.replyNotUnderstood(msg);
         }
     };
+
+    private void handlePurchaseOrder(ACLMessage msg) throws UnreadableException {
+        PurchaseOrder purchaseOrder = (PurchaseOrder) msg.getContentObject();
+        log.info("Handling purchaseOrder from " + purchaseOrder.getCustomerAgentId());
+        purchaseOrders.add(purchaseOrder);
+        ACLMessage reply = msg.createReply();
+        reply.setPerformative(ACLMessage.ACCEPT_PROPOSAL);
+        send(reply);
+        log.info("Purchase order handled");
+    }
 }
