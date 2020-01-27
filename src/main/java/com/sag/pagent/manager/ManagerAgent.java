33f3166ef8e7c439561f12839149d19d206afe1a
Wasiollo <wasiak.mk@gmail.com>
2020-01-27 23:22:41 +0100

    Adding purchaseOrders to list in ManagerAgent

--- src/main/java/com/sag/pagent/manager/ManagerAgent.java
+++ src/main/java/com/sag/pagent/manager/ManagerAgent.java
@@ -2,8 +2,10 @@ package com.sag.pagent.manager;
 
 import com.sag.pagent.agents.BasicAgent;
 import com.sag.pagent.behaviors.ReceiveMessagesBehaviour;
+import com.sag.pagent.customer.order.OrderArticle;
 import com.sag.pagent.manager.messages.PurchaseOrder;
 import com.sag.pagent.services.ServiceType;
+import com.sag.pagent.shop.articles.ArticleType;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
@@ -11,12 +13,16 @@ import jade.lang.acl.UnreadableException;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 public class ManagerAgent extends BasicAgent {
     private ReceiveMessagesBehaviour receiveMessages;
     private List<PurchaseOrder> purchaseOrders = new ArrayList<>();
+    private Double budget = 0d;
+    private Map<ArticleType, Integer> articlesToBuy = new EnumMap<>(ArticleType.class);
 
     @Override
     protected void addServices(DFAgentDescription dfd) {
@@ -54,9 +60,25 @@ public class ManagerAgent extends BasicAgent {
         PurchaseOrder purchaseOrder = (PurchaseOrder) msg.getContentObject();
         log.info("Handling purchaseOrder from " + purchaseOrder.getCustomerAgentId());
         purchaseOrders.add(purchaseOrder);
+        budget += purchaseOrder.getBudget();
+        addArticlesToBuy(purchaseOrder.getArticlesToBuy());
         ACLMessage reply = msg.createReply();
         reply.setPerformative(ACLMessage.ACCEPT_PROPOSAL);
         send(reply);
-        log.info("Purchase order handled");
+        log.info("Handled Purchase order from conversation: " + msg.getConversationId());
+        log.debug("Budget: " + budget);
+    }
+
+    private void addArticlesToBuy(List<OrderArticle> articles) {
+        articles.forEach(article -> {
+            if (articlesToBuy.containsKey(article.getArticle())) {
+                Integer articleAmount = articlesToBuy.get(article.getArticle()) + article.getAmount();
+                articlesToBuy.put(article.getArticle(), articleAmount);
+                log.debug("Article existed, amount set to :" + articleAmount);
+            } else {
+                articlesToBuy.put(article.getArticle(), article.getAmount());
+                log.debug("Article didn't exist, amount set to: " + article.getAmount());
+            }
+        });
     }
 }
